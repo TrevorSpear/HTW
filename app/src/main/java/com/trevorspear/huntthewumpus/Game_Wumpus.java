d68c2bcc4016747b2dc86484b929c919baa107c1
Trevor Spear <TrevorSpear@Rebeccas-MacBook-Pro.local>
2017-06-22 20:19:36 -0700

    Started next layout

---
+++ app/src/main/java/com/trevorspear/huntthewumpus/Game_Wumpus.java
@@ -0,0 +1,20 @@
+package com.trevorspear.huntthewumpus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TrevorSpear on 6/22/17.
+ */
+
+public class Game_Wumpus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+
+
+}
